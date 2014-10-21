9292f8e631df998cbc14ba75a6e9dbdff916e555
Daniel Steinberg <daniel@dimsumthinking.com>
2014-10-21 12:31:38 +0100

    Non homogeneous arrays - don't do this

---
+++ Collections1.playground/section-1.swift
@@ -0,0 +1,23 @@
+var numbers = [1, 2, 3]
+
+numbers[1] = 7
+
+numbers
+
+numbers[1...2] = [5,6,7,8,9,10]
+
+numbers.append(576)
+
+numbers += [1002]
+
+numbers.insert(900, atIndex: 3)
+
+numbers.removeLast()
+
+numbers.removeAtIndex(0)
+
+
+numbers.removeAll(keepCapacity: true)
+numbers
+
+//numbers.append("Iguana")
---
+++ Collections2.playground/section-1.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+let numbers = [0, 1, 2,3]
+
+let one = numbers[1]
+let two = numbers[2]
+
+let three = one + two
+
+
+
+let altOne = mixedArray[1]
+
+let altTwo = mixedArray[2]
+
+//let altThree = altOne + altTwo
