42e5359871889f15e8e7d7dc40e6c05fa96210ad
Daniel Steinberg <daniel@dimsumthinking.com>
2014-10-21 14:36:39 +0100

    Generics

--- MoreFunctions5.playground/section-1.swift
+++ MoreFunctions5.playground/section-1.swift
@@ -1,8 +1,9 @@
 var numbers = [5, 2, 8, 3, 9, 4]
 var doubles = [5.0, 2.0, 8.0, 3.0, 9.0, 4.0]
+var strings = ["Five", "Two", "Eight", "Three", "Nine", "Four"]
 
-func emphasize(inout array: [Int],
-    modificationOf:(Int) -> Int) {
+func emphasize<T>(inout array: [T],
+                    modificationOf:(T) -> T) {
         for i in 0 ..< array.count {
             array[i] = modificationOf( array[i] )
         }
@@ -13,5 +14,10 @@ func times100(number:Int) -> Int {
 }
 
 emphasize(&numbers){number in number * 100}
-
 numbers
+
+emphasize(&doubles){number in number * 100.0}
+doubles
+
+emphasize(&strings){string in string + "!"}
+strings
