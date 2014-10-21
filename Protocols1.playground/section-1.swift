c0018679d7b71a80c921e3dfb141bfd3e1294cd7
Daniel Steinberg <daniel@dimsumthinking.com>
2014-10-21 17:10:36 +0100

    polymorphism

--- Protocols1.playground/section-1.swift
+++ Protocols1.playground/section-1.swift
@@ -1,5 +1,58 @@
-// Playground - noun: a place where people can play
+protocol Movable {
+    func moveByX(x: Double) -> Self
+}
 
-import UIKit
+func shiftLeft(movable:Movable) -> Movable {
+    return movable.moveByX(-1)
+}
 
-var str = "Hello, playground"
+struct Vertex: Movable {
+    var x, y: Double
+    
+    func moveByX(deltaX: Double) -> Vertex {
+        return Vertex(x: x + deltaX, y: y)
+    }
+}
+
+struct Size {
+    var width, height: Double
+}
+
+struct Rectangle: Movable {
+    var size: Size
+    var topLeftCorner: Vertex
+    
+    func moveByX(x: Double) -> Rectangle {
+        let movedTopLeftCorner = topLeftCorner.moveByX(x)
+        return Rectangle(size: size,
+                topLeftCorner: movedTopLeftCorner)
+    }
+}
+
+var point = Vertex(x: 3.0, y: 4.0)
+let shiftedPoint = point.moveByX(4)
+let rectangle = Rectangle(size: Size(width: 200, height: 100), topLeftCorner: point)
+
+let shiftedRectangle = shiftLeft(rectangle)
+
+if let shiftedLeftPoint = shiftLeft(point) as? Vertex {
+    let shiftedRectangle2 = Rectangle(size: rectangle.size,
+        topLeftCorner: shiftedLeftPoint)
+} else {
+    // nothing
+}
+point
+shiftedPoint
+
+
+
+
+
+
+
+
+
+
+
+
+
