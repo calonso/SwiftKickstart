f2d8f580da93f8c4302fcda5af786695a49eadf0
Daniel Steinberg <daniel@dimsumthinking.com>
2014-10-21 14:46:41 +0100

    extensions

--- MoreFunctions6.playground/section-1.swift
+++ MoreFunctions6.playground/section-1.swift
@@ -1,5 +1,26 @@
-// Playground - noun: a place where people can play
+extension Array {
+    mutating func emphasize(modificationOf:(T) -> T) {
+        for i in 0 ..< self.count {
+            self[i] = modificationOf( self[i] )
+        }
+    }
+}
 
-import UIKit
+var numbers = [5, 2, 8, 3, 9, 4]
+var doubles = [5.0, 2.0, 8.0, 3.0, 9.0, 4.0]
+var strings = ["Five", "Two", "Eight", "Three", "Nine", "Four"]
 
-var str = "Hello, playground"
+
+func times100(number:Int) -> Int {
+    return number * 100
+}
+
+
+numbers.emphasize{number in number * 10}
+numbers
+
+doubles.emphasize{number in number * 100.0}
+doubles
+
+strings.emphasize{string in string + "!"}
+strings
