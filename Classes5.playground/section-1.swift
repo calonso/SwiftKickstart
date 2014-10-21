f2cb8674b0761a14e889e479681b12c36f81da6e
Daniel Steinberg <daniel@dimsumthinking.com>
2014-10-21 15:18:24 +0100

    computed property

--- Classes5.playground/section-1.swift
+++ Classes5.playground/section-1.swift
@@ -1,5 +1,37 @@
-// Playground - noun: a place where people can play
+class CocoaConfAttendee  {
+    let name: String
+    let hometown: String?
+    var isFromSomewhere: Bool {
+        return hometown != nil
+    }
+    
+    init(name: String, hometown: String) {
+        self.name = name
+        self.hometown = hometown
+    }
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func nameBadge() -> String {
+        var validHometown: String
+        if isFromSomewhere {
+            validHometown = " from \(hometown!)"
+        } else {
+            validHometown = ""
+        }
+        return "Hello, I'm \(name) \(validHometown)."
+        
+    }
+}
 
-import UIKit
+let daniel = CocoaConfAttendee(name: "Daniel", hometown: "Cleveland")
+daniel.name
+daniel.hometown
+daniel.nameBadge()
 
-var str = "Hello, playground"
+let kim = CocoaConfAttendee(name: "Kim")
+kim.name
+kim.hometown
+kim.nameBadge()
