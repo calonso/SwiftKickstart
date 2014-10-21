24e5d2423fc32471646d5793e6524a5468dc241a
Daniel Steinberg <daniel@dimsumthinking.com>
2014-10-21 16:26:22 +0100

    by value vs by reference

--- Structs2.playground/section-1.swift
+++ Structs2.playground/section-1.swift
@@ -1,5 +1,39 @@
-// Playground - noun: a place where people can play
+struct Vertex {
+    var x, y :Double
+}
 
-import UIKit
+var point = Vertex(x: 3.0, y: 4.0)
+point.x
+point.y
 
-var str = "Hello, playground"
+var anotherPoint = point
+
+point.x = 100
+anotherPoint.y = 1000
+
+point
+anotherPoint
+
+
+class VertexAlt {
+    var x, y: Double
+    init(x:Double, y:Double) {
+        self.x = x
+        self.y = y
+    }
+}
+
+var pointA = VertexAlt(x: 3.0, y: 4.0)
+var pointB = pointA
+
+pointA.x = 100.0
+pointB.y = 1000.0
+
+pointA
+pointB
+
+
+
+
+
+
---
+++ Structs3.playground/section-1.swift
@@ -0,0 +1,7 @@
+struct Vertex {
+    var x, y :Double
+}
+
+var point = Vertex(x: 3.0, y: 4.0)
+point.x
+point.y
